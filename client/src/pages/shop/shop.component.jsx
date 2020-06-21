baad7f96c3def21f8bf17bebdca45797a53dcf6d
NUTT CHOKWITTAYA <15667217+ACHEER1420@users.noreply.github.com>
2020-06-20 20:35:24 -0500

    [Lazy load + Suspense] - implement lazy load on all nneded routes in shop.component.jsx

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,30 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 
 // Components
+import Spinner from '../../components/spinner/spinner.component';
 import { Route } from 'react-router-dom';
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../../pages/collection/collection.container';
 
 // Functions from actions not saga, saga will listen to this action too
 import { fetchCollectionStart } from '../../redux/shop/shop.actions';
 import { connect } from 'react-redux';
 
+const CollectionsOverviewContainer = lazy(() =>
+  import('../../components/collections-overview/collections-overview.container')
+);
+const CollectionPageContainer = lazy(() =>
+  import('../../pages/collection/collection.container')
+);
+
 const Shop = ({ match, fetchCollectionStart }) => {
   useEffect(() => {
     fetchCollectionStart();
   }, [fetchCollectionStart]);
-  
+
   return (
     <div className='shop-page'>
-      <Route
-        exact
-        path={`${match.path}`}
-        component={CollectionsOverviewContainer}
-      />
-      <Route
-        path={`${match.path}/:collectionId`}
-        component={CollectionPageContainer}
-      />
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
     </div>
   );
 };
